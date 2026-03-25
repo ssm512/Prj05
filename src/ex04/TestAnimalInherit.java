b30dd8993e768313ffcf7c7aab61a057d339b56a
ssm512 <shinsang512@naver.com>
2026-03-25 14:53:25 +0900

    0325 3rd commit Pet class abstract 활용 예제

--- src/ex04/TestAnimalInherit.java
+++ src/ex04/TestAnimalInherit.java
@@ -19,25 +19,78 @@ class Cat extends Pet {
 	}
 } // class Cat end
 
+class Tiger	extends Pet {
+	void			cry() {
+		System.out.println(name + "(이)가 어흥");
+	}
+} // class Tiger end
+
 public class TestAnimalInherit {
 	
 	// 상속 : Inherit -> java에서는 extends로 처리
 	// 다중상속 불가능, 상속은 한개만 가능 -> java, c#
 	public static void main(String[] args) {
-		Pet		pet		=	new Pet();
-		pet.name		=	"뻐끔이";
-		pet.eat();
-		
 		
 		Dog		dog		=	new Dog();
-		dog.name		=	"츄";
-		dog.eat();
-		dog.bark();
+		work(dog,"츄");
 		
+//		dog.name		=	"츄";
+//		dog.eat();
+//		dog.bark();
+		System.out.println("---------");
+
+	
 		Cat			nero	=	new Cat();
-		nero.name			=	"네로";
-		nero.eat();
-		nero.meow();
+		work(nero,"네로");
+//		nero.name			=	"네로";
+//		nero.eat();
+//		nero.meow();
+		System.out.println("---------");
+
+		Pet		pet			=	new Pet();
+		pet.name			=	"뻐끔이";
+		pet.eat();
+//		pet.bark();	//	error	
+//		pet.meow();	//	error
+		System.out.println("==========");
+		
+		Tiger	tiger		=	new Tiger();
+		work(tiger, "큰냥이");
 	} // main end
 
+	// 한개의 work() 함수로 업무 처리
+	// 방법2
+	private static void work(Pet pet, String name) {
+		pet.name			=	name;
+		System.out.println("work:");
+		pet.eat();
+		
+		if (pet instanceof Dog)
+			((Dog) pet).bark();
+		if (pet instanceof Cat)
+			((Cat) pet).meow();
+		if (pet instanceof Tiger)
+			((Tiger) pet).cry();
+	} // work() 함수 생성
+
+	
+/*
+ 	// 방법 1
+	private static void work(Cat nero, String name) {
+		nero.name			=	name;
+		System.out.print("work:");
+		nero.eat();
+		nero.meow();
+		System.out.println("**");
+		
+	} // Cat의 work 생성
+
+	private static void work(Dog dog, String name) {
+		dog.name			=	name;
+		System.out.print("work:");
+		dog.eat();
+		dog.bark();
+		System.out.println("*");
+	} // work method 생성
+*/
 } // class TestAnimalInherit
---
+++ src/ex05/TestAnimal.java
@@ -0,0 +1,84 @@
+package ex05;
+
+abstract class Pet { // 한개이상ㅇ의 abstract 메소드가 있다면  abstract class로 만들어야 한다
+	String			name;
+	void			eat() {
+		System.out.println(name+"(이)가 먹는다");
+	}
+	abstract void			sound(); 	// 함수의 {}가 없으면 abstract 필수
+}
+
+class Dog extends Pet {
+	// 코딩이 없는 함수를 상속받으면 반드시 자식이 코딩을 구현 해야 한다 - 재정의(override)
+	// Add uniplemented method
+	@Override		// 재정의하다
+	void sound() {
+		System.out.println(name + "(이)가 멍멍");
+	} // sound() end
+	
+}
+
+class Cat extends Pet { // Pet 상속을 안 받으면 sound override 안 해도 됨
+	@Override
+	void sound() {
+		System.out.println(name + "(이)가 야옹");		
+	} // sound () end
+		
+}
+
+public class TestAnimal {
+
+	public static void main(String[] args) {
+		
+//		Pet 	pet			=	new Pet();	//	abstract가 있으면 new 불가능
+		
+		Dog		dog1		=	new	Dog();
+		work(dog1, "츄");
+//		dog1.name		=	"츄";
+//		dog1.eat();
+//		dog1.sound();
+
+		Cat		cat1	=	new	Cat();
+		work(cat1, "네로");
+//		cat1.name		=	"네로";
+//		cat1.eat();
+//		cat1.sound();
+		
+	} // main end
+
+	
+	//----------------------------------------------------------------
+	// work 한개로 통일
+	// 부모클래스는 자식클래스를 담을 수 없다
+	// Pet pet = new Pet(); Dog dog1 = pet; // 문법오류임
+	// 자식클래스는 부모클래스를 담을 수 있다
+	// Pet pet = dog1;
+	// 방법2. 
+	private static void work(Pet pet, String name) {
+		pet.name	=	name;
+		pet.eat();
+		pet.sound();
+		
+	} // work (); end
+
+	//----------------------------------------------------------------
+/*
+	// 방법1 : 함수의 오버로드 기능 활용
+	private static void work(Dog dog1, String name) {
+		dog1.name		=	name;
+		dog1.eat();
+		dog1.sound();
+	}
+
+	private static void work(Cat cat1, String name) {
+		cat1.name		=	name;
+		cat1.eat();
+		cat1.sound();
+	}
+*/
+	//----------------------------------------------------------------
+		
+		
+	
+
+} // class TestAnimal
